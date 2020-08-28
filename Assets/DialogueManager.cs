6e90729fb68bb30ff077163511643de671c50270
Registrium <Registrium@gmail.com>
2020-08-29 02:52:22 +0900

    Initial Commit

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text Narrator, Dialogue;
+    private int a = 0, i;
+    private string c = "1", SceneNum = "1-1", DialogueText = "";
+    private bool isGoing;
+    string[] Line, Info;
+
+    void Start()
+    {
+        // 파일의 이름, 줄 수를 불러옵니다.
+        
+        //SceneNum = dataController.instance.Load(0);
+        //c = dataController.instance.Load(1);
+
+        Line = File.ReadAllLines("Assets/Scenario/"+ SceneNum + ".txt");
+        isGoing = true;
+        StartCoroutine(DialogueGoing());
+    }
+
+    IEnumerator DialogueGoing() {
+        
+        if (isGoing) { // 대화가 진행중일때
+            Info = Line[a].Split('/'); // 분리
+            Narrator.text = Info[0]; // 등장인물 이름
+            DialogueText = ""; // 텍스트 내용 초기화
+            for(i = 0; i < Info[1].Length; i++) {
+                DialogueText += Info[1][i];
+                Dialogue.text = DialogueText;
+                yield return new WaitForSeconds(0.1f);
+            }
+        } else { // isGoing = True;
+            yield return null;
+        }
+    } 
+
+    void Update() // isGoing = false, 기본 = 대화는 진행중이지 않다
+    {
+        
+        if (!isGoing && Input.GetMouseButtonDown(0)) {
+            // 대화 진행중이지 않을 때 클릭하면 다음 대화 출력
+            a++;
+            isGoing = true; // 대화는 진행중이다
+            StartCoroutine(DialogueGoing());
+        } else if (isGoing && Input.GetMouseButtonDown(0)) { // isGoing = True
+            // 대화 진행중이면 이번 대화를 강제로 출력
+            isGoing = false;
+            i = Info[1].Length; // for문 강제 끝
+            Dialogue.text = Info[1]; // 강제 출력
+        }
+    }
+}
+
+// 지금 문제
+
+// 타이핑 중 클릭하면 바로 다음으로 진행
+
+// (타이핑 취소하고 새로 만든 다음 덮어쓰기)
---
+++ Assets/dataController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class dataController : MonoBehaviour
+{
+    //StreamWriter sw = new StreamWriter("Assets/Scenario/ScenarioInfo.txt");
+    public static dataController instance;
+    public GameObject Option, Dialogue, NonDialogue, NonOption;
+    public RawImage BlackTheme;
+    private string[] str;
+    private float MVolume, BGVolume, EVolume; // 마스터, 배경, 효과 음악 소리 크기
+    private float H1Voice, H2Voice, H3Voice, H4Voice, OtherVoice; // 히로인별 음성 크기
+    private float SubSpeed, AutoSpeed, TransparentDegree; // 자막 속도, 오토모드 자막 속도, 창 투명 정도
+
+    // fade 구현!
+    float fades = 0.0f;
+
+    public void OptionOpen() {
+        StartCoroutine(OptionOpener());
+    }
+
+    public void OptionClose() {
+        StartCoroutine(OptionCloser());
+    }
+
+    IEnumerator OptionOpener() {
+        while(true) {
+            fades += 0.1f;
+            BlackTheme.color = new Color (0, 0, 0, fades);
+            if (fades >= 1.0f) {
+                break;
+            } else {
+                yield return new WaitForSeconds(0.04f);
+            }
+        }
+        NonDialogue.SetActive(false);
+        Dialogue.SetActive(false);
+        Option.SetActive(true);
+        NonOption.SetActive(true);
+        while(true) {
+            fades -= 0.1f;
+            BlackTheme.color = new Color (0, 0, 0, fades);
+            if (fades <= 0.0f) {
+                break;
+            } else {
+                yield return new WaitForSeconds(0.04f);
+            }
+        }
+        yield return null;
+    }
+
+    IEnumerator OptionCloser() {
+        while(true) {
+            fades += 0.1f;
+            BlackTheme.color = new Color (0, 0, 0, fades);
+            if (fades >= 1.0f) {
+                break;
+            } else {
+                yield return new WaitForSeconds(0.04f);
+            }
+        }
+        Option.SetActive(false);
+        NonOption.SetActive(false);
+        Dialogue.SetActive(true);
+        NonDialogue.SetActive(true);
+        while(true) {
+            fades -= 0.1f;
+            BlackTheme.color = new Color (0, 0, 0, fades);
+            if (fades <= 0.0f) {
+                break;
+            } else {
+                yield return new WaitForSeconds(0.04f);
+            }
+        }
+        yield return null;
+    }
+
+    void Awake () {
+        instance = this; // 사용법 = dataController.instance.변수 = 뭐시기뭐시기~
+    }
+
+    // 1번 저장법 PlayerPrefs
+    // 2번 저장법 json / XML
+
+    // 1번 저장법
+
+    //public void Save(string SceneNum, string a) {
+        //File.Delete("Assets/Scenario/ScenarioInfo.txt");
+        //FileStream fs = File.Create("Assets/Scenario/ScanrioInfo.txt");
+        //sw.Write(SceneNum + " " + a);
+        //sw.Close();
+    //}
+
+    //public string Load(int num) { // 문제의 부분 , void->string
+        //str = File.ReadAllLines("Assets/Scenario/ScenarioInfo.txt");
+        //string[] words = str[0].Split('/');
+        // string[] words = lines[num1].Split('/');
+        //if (num == 0) {
+            //return words[0]; // 저장 되어있는 SceneNum 반환
+        //} else {
+            //return words[1]; // 저장 되어있는 a값 반환
+        //}
+    //}
+    
+}
